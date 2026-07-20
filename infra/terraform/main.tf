863bb4bba302cd95343200e9f9d39ed0cf30babc
kishoreismac <27953059+kishoreismac@users.noreply.github.com>
2026-07-20 12:33:22 +0530

    Fix App Service health check configuration

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -70,6 +70,7 @@ resource "azurerm_linux_web_app" "api" {
     minimum_tls_version                     = "1.2"
     ftps_state                              = "Disabled"
     health_check_path                       = "/health"
+    health_check_eviction_time_in_min       = 5
 
     application_stack {
       docker_image_name   = "cpp-api:${var.api_image_tag}"
@@ -117,6 +118,7 @@ resource "azurerm_linux_web_app" "web" {
     minimum_tls_version                     = "1.2"
     ftps_state                              = "Disabled"
     health_check_path                       = "/"
+    health_check_eviction_time_in_min       = 5
 
     application_stack {
       docker_image_name   = "cpp-web:${var.web_image_tag}"
